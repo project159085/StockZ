845c863cbe5ccff2ea0e191c968874a96c43c216
project159085 <project159085@gmail.com>
2025-11-23 00:21:06 +0530

    Creating Home Component Part-3

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+    return (
+        <div className='container p-5 mb-5'>
+            <div className='row text-center'>
+                <img src='media/images/homeHero.png' alt='Hero Img' className='mb-5' />
+                <h1 className='mt-5'>Invest In Everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds and more</p>
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width: "15%", margin: "auto"}}>Signup Now</button>
+            </div>
+        </div>
+    );
 }
 
 export default Hero;
