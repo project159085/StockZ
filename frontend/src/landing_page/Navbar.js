947d07c5c716b68eb0d451b825d4713232012a63
project159085 <project159085@gmail.com>
2025-11-26 23:37:57 +0530

    Created Navbar Section in Homepage

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -2,11 +2,58 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div>
-      <div>
-        <div></div>
-      </div>
-    </div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom">
+        <div class="container p-2">
+          <a class="navbar-brand" href="#">
+            <img src="media/images/logo.svg" alt="logo" style={{width: "25%"}} />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <form class="d-flex">
+              <ul class="navbar-nav mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Signup
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Product
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Support
+                </a>
+              </li>
+              <li class="nav-item">
+                <i class="nav-link fa fa-bars mt-1" aria-hidden="true"></i>
+              </li>
+            </ul>
+            </form>
+          </div>
+        </div>
+      </nav>
   );
 }
 
