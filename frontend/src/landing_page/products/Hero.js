e244fe30ece5fb409683b329e326d03c32c2dd73
project159085 <project159085@gmail.com>
2025-12-01 17:22:44 +0530

    Implemented Left Section of Products Page

--- frontend/src/landing_page/products/Hero.js
+++ frontend/src/landing_page/products/Hero.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( 
-        <div>
-            <h1>Hero</h1>
-        </div>
-     );
+  return (
+    <div className="container">
+      <div className="row text-center mt-5 p-3">
+        <h1>StockZ Products</h1>
+        <h4 className="text-muted mt-3">Sleek, modern and intuitive trading platforms</h4>
+        <p className="mt-3">
+          Checkout our{" "}
+          <a href="#" style={{ textDecoration: "none" }}>
+            investment offerings{" "}
+            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
