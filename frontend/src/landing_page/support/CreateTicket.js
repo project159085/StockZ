80d5a619dec01368701fe1eda28f8b24b36d94ea
project159085 <project159085@gmail.com>
2025-12-11 21:05:26 +0530

    Created Support component part-1

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
 
 function CreateTicket() {
-    return ( 
-        <div>
-            <h1>CreateTicket</h1>
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2">To create a ticket, select a relevant topic</h1>
+        <div className="col-4 p-5 mt-5 mb-5">
+          <h4 className="">
+            <i class="fa fa-plus-circle" aria-hidden="true"></i>
+            Account Opening
+          </h4>
         </div>
-     );
+        <div className="col-4 p-5 mt-5 mb-5">
+          <h4 className="">Your StockZ account</h4>
+        </div>
+        <div className="col-4 p-5 mt-5 mb-5">
+          <h4 className="">Your StockZ account</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
